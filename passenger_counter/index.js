d7ee2fa8e3e17bcaa71ca61f0182cd929271703b
Ulopa94 <38452203+Ulopa94@users.noreply.github.com>
2022-03-03 22:23:59 +0300

    first commit

---
+++ passenger_counter/index.js
@@ -0,0 +1,15 @@
+// document.getElementById("counter").innerText = 5
+
+// app to-do
+
+// initialize count to 0
+
+// listen for button clicks
+
+// increment count on click
+
+// update the count value
+
+function increment() {
+    console.log("Yes!!Button clicked")
+}
