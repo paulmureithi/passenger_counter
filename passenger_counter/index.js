d61cf88b142d928d188d007ea16a2666f65be9fd
Ulopa94 <38452203+Ulopa94@users.noreply.github.com>
2022-03-04 00:16:21 +0300

    added a save feature

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -2,10 +2,15 @@ let count = 0
 
 // listen for button clicks and increment count on click
 function increment() {
-    count = count+1
+    count += 1
     // console.log(count)
     document.getElementById("counter").innerText = count
 }
 
-
+function save() {
+    let counter = count + " - "
+    document.getElementById("save-el").textContent += counter
+    count = 0
+    document.getElementById("counter").innerText = count
+}
 
